d693d776f70ddab1eb65cd09efa79ea06eafa164
Rokas Jasonas <rokas.jasonas@gmail.com>
2026-09-05 08:25:04 +0300

    Bump version to 0.2.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "lt.carfinder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
